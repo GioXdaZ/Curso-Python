8bf821e93f062b572a4b0a99f881fbb09130a72a
GioXdaZ <gio.daz@gmail.com>
2024-04-09 16:43:46 -0400

    First commit

---
+++ tipos de datos/datos-simples.py
@@ -0,0 +1,20 @@
+"string"
+'string'
+
+"""tus datos son:
+    nombre: lucas
+    apellido: dalto"""
+    
+'''tus datos son:
+    nombre: lucas
+    apellido: dalto'''
+    
+    
+# int o entero
+40
+# float o flotante
+40.2
+
+# boolean
+True
+False
---
+++ variables/variables.py
@@ -0,0 +1,18 @@
+
+# definiendo una variable con camelCase
+nombreCompleto = 'Giovanni Dazarola'
+
+#RECOMENDADO POR PYTHON
+# difiniendo una vriable con snake_case
+nombre_completo = 'Giovanni Dazarola'
+
+# concatenar con +
+bienvenida = 'hola ' + nombre_completo + ', ¿como estas?'
+
+# concatenar con f-string
+bienvenida = f'hola {nombre_completo}, ¿como estas?'
+
+# operadore de pertenecia (in / not in)
+
+print('Giovanni' in bienvenida) #true
+print('Giovanni' not in bienvenida) #false
